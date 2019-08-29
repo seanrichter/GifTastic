2975881db58cdc8a7471b2fff6bb8ad9c661eb75
Sean Richter <sean.richter04@outlook.com>
2019-08-28 22:14:45 -0500

    javascript and jquery added.  somethiing wrong

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,59 @@
 
+// Example queryURL for Giphy API
+var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=SDGwxtRf1A6xMC2lDSGFdc5Z6npokhMP";
+
+queryURL.done(function(response) { console.log("success", response); });
+
+var gifs = response.data
+
+for (i in gifs){
+    $('.inner').append('<img>')
+}
+
+// event listener for all button elements
+$("button").on("click", function() {
+    // "this" keyword refers to the button that was clicked
+    var animal = $(this).attr("data-animal");
+
+    // URL to search Giphy for the animal button that was clicked.
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+    animal + "api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9";
+
+    // AJax GET request
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+
+    //Data comes back from API
+    .then(function(response) {
+        var results = response.data;
+        console.log(response);
+
+    // looping the results
+    for (var i = 0; i < results.length; i++) {
+        // checking for photos rating.  posting appropriate gifs
+        if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+            //making div for gif
+            var divGif = $("<div>");
+
+            // storing result ratings
+            var rating = results[i].rating;
+
+            //making a <p> tag with results rating
+            var p = $("<p>").text("Rating: " + rating);
+
+            // making an img tag
+            var animalImage = $("<img>");
+
+            animalImage.attr("src", results[i].images.fixed_height.url);
+
+            divGif.append(p);
+            divGif.append(animalImage);
+
+            $("gifAppear").prepend(divGif);
+            
+        }
+    }
+    });
+});
