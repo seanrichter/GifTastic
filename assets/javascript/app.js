dd194851a49aeb72201cbc7c2c89b29b3b6d3404
Sean Richter <sean.richter04@outlook.com>
2019-09-24 18:26:54 -0500

    fixed indentation

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,41 +1,41 @@
-var topics = ["Cowboys","Patriots","Eagles","Raiders","Packers","Steelers","Bears","49ers","Giants","Browns","Vikings",
-"Chiefs","Colts","Broncos","Texans","Bills","Saints","Redskins","Dolphins","Jets","Cardinals","Lions","Ravens","Rams",
-"Panthers","Chargers","Falcons","Buccaneers","Titans","Bengals","Jaguars"];
+var topics = ["Cowboys", "Patriots", "Eagles", "Raiders", "Packers", "Steelers", "Bears", "49ers", "Giants", "Browns", "Vikings",
+	"Chiefs", "Colts", "Broncos", "Texans", "Bills", "Saints", "Redskins", "Dolphins", "Jets", "Cardinals", "Lions", "Ravens", "Rams",
+	"Panthers", "Chargers", "Falcons", "Buccaneers", "Titans", "Bengals", "Jaguars"];
 var numberOfGIFs = 10;
 var rating = "PG";
-function renderButtons(){
-    for(var i = 0; i < topics.length; i++) {
-        var newButton = $("<button>");
-        newButton.addClass("btn");
-        newButton.addClass("team-button");
-        newButton.text(topics[i]);
-        $("#button-container").append(newButton);
-    }
-    $(".team-button").unbind("click");
+function renderButtons() {
+	for (var i = 0; i < topics.length; i++) {
+		var newButton = $("<button>");
+		newButton.addClass("btn");
+		newButton.addClass("team-button");
+		newButton.text(topics[i]);
+		$("#button-container").append(newButton);
+	}
+	$(".team-button").unbind("click");
 
-    $(".team-button").on("click", function(){
+	$(".team-button").on("click", function () {
 
-        $(".gif-image").unbind("click");
-        $("#gif-container").empty();
-        $("#gif-container").removeClass("border");
+		$(".gif-image").unbind("click");
+		$("#gif-container").empty();
+		$("#gif-container").removeClass("border");
 		populateGIFContainer($(this).text());
 	});
 }
 
-function addButton(team){
-	if(topics.indexOf(team) === -1) {
+function addButton(team) {
+	if (topics.indexOf(team) === -1) {
 		topics.push(team);
 		$("#button-container").empty();
 		renderButtons();
 	}
 }
 
-function populateGIFContainer(team){
-    $.ajax({
+function populateGIFContainer(team) {
+	$.ajax({
 		url: "https://api.giphy.com/v1/gifs/search?api_key=gO69tn1Qwb1dMmTpAwcc4Pb7GNWN1n5j&q=football&limit=10&offset=0&rating=PG&lang=en" + team + rating + numberOfGIFs,
 		method: "GET"
-    }).then(function(response){
-		response.data.forEach(function(element){
+	}).then(function (response) {
+		response.data.forEach(function (element) {
 			newDiv = $("<div>");
 			newDiv.addClass("individual-gif-container");
 			newDiv.append("<p>rating: " + element.rating.toUpperCase() + "</p>");
@@ -50,8 +50,8 @@ function populateGIFContainer(team){
 
 		$("#gif-container").addClass("border");
 		$(".gif-image").unbind("click");
-		$(".gif-image").on("click", function(){
-			if($(this).attr("state") === "still") {
+		$(".gif-image").on("click", function () {
+			if ($(this).attr("state") === "still") {
 				$(this).attr("state", "animated");
 				$(this).attr("src", $(this).attr("animated-data"));
 			}
@@ -63,9 +63,9 @@ function populateGIFContainer(team){
 	});
 }
 
-$(document).ready(function(){
+$(document).ready(function () {
 	renderButtons();
-	$("#submit").on("click", function(){
+	$("#submit").on("click", function () {
 		event.preventDefault();
 		addButton($("#team").val());
 		$("#team").val("");
