0b2250b7bbc5cebafe2724fcf327d5423e8bcdd3
Sean Richter <sean.richter04@outlook.com>
2020-02-03 19:30:36 -0600

    update readme

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # GifTastic
-use the GIPHY API to make a dynamic web page that populates with gifs of your choice.
-https://seanrichter.github.io/GifTastic/
+In this assignment, you'll use the GIPHY API to make a dynamic web page that populates with gifs of your choice. To finish this task, you must call the GIPHY API and use JavaScript and jQuery to change the HTML of your site.
+
+## Key Topics
+* JSON
+* Ajax
+* HTTP GET requests
+* Server-side APIs
+
+[GifTastic](https://seanrichter.github.io/GifTastic/)
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,73 +1,107 @@
-var topics = ["Cowboys", "Patriots", "Eagles", "Raiders", "Packers", "Steelers", "Bears", "49ers", "Giants", "Browns", "Vikings",
-	"Chiefs", "Colts", "Broncos", "Texans", "Bills", "Saints", "Redskins", "Dolphins", "Jets", "Cardinals", "Lions", "Ravens", "Rams",
-	"Panthers", "Chargers", "Falcons", "Buccaneers", "Titans", "Bengals", "Jaguars"];
+var topics = [
+  "Cowboys",
+  "Patriots",
+  "Eagles",
+  "Raiders",
+  "Packers",
+  "Steelers",
+  "Bears",
+  "49ers",
+  "Giants",
+  "Browns",
+  "Vikings",
+  "Chiefs",
+  "Colts",
+  "Broncos",
+  "Texans",
+  "Bills",
+  "Saints",
+  "Redskins",
+  "Dolphins",
+  "Jets",
+  "Cardinals",
+  "Lions",
+  "Ravens",
+  "Rams",
+  "Panthers",
+  "Chargers",
+  "Falcons",
+  "Buccaneers",
+  "Titans",
+  "Bengals",
+  "Jaguars"
+];
 var numberOfGIFs = 10;
 var rating = "PG";
 function renderButtons() {
-	for (var i = 0; i < topics.length; i++) {
-		var newButton = $("<button>");
-		newButton.addClass("btn");
-		newButton.addClass("team-button");
-		newButton.text(topics[i]);
-		$("#button-container").append(newButton);
-	}
-	$(".team-button").unbind("click");
+  for (var i = 0; i < topics.length; i++) {
+    var newButton = $("<button>");
+    newButton.addClass("btn");
+    newButton.addClass("team-button");
+    newButton.text(topics[i]);
+    $("#button-container").append(newButton);
+  }
+  $(".team-button").unbind("click");
 
-	$(".team-button").on("click", function () {
-
-		$(".gif-image").unbind("click");
-		$("#gif-container").empty();
-		$("#gif-container").removeClass("border");
-		populateGIFContainer($(this).text());
-	});
+  $(".team-button").on("click", function() {
+    $(".gif-image").unbind("click");
+    $("#gif-container").empty();
+    $("#gif-container").removeClass("border");
+    populateGIFContainer($(this).text());
+  });
 }
 
 function addButton(team) {
-	if (topics.indexOf(team) === -1) {
-		topics.push(team);
-		$("#button-container").empty();
-		renderButtons();
-	}
+  if (topics.indexOf(team) === -1) {
+    topics.push(team);
+    $("#button-container").empty();
+    renderButtons();
+  }
 }
 
 function populateGIFContainer(team) {
-	$.ajax({
-		url: "https://api.giphy.com/v1/gifs/search?api_key=gO69tn1Qwb1dMmTpAwcc4Pb7GNWN1n5j&q=football&limit=10&offset=0&rating=PG&lang=en" + team + rating + numberOfGIFs,
-		method: "GET"
-	}).then(function (response) {
-		response.data.forEach(function (element) {
-			newDiv = $("<div>");
-			newDiv.addClass("individual-gif-container");
-			newDiv.append("<p>rating: " + element.rating.toUpperCase() + "</p>");
-			var newImage = $("<img src = '" + element.images.fixed_height_still.url + "'>");
-			newImage.addClass("gif-image");
-			newImage.attr("state", "still");
-			newImage.attr("still-data", element.images.fixed_height_still.url);
-			newImage.attr("animated-data", element.images.fixed_height.url);
-			newDiv.append(newImage);
-			$("#gif-container").append(newDiv);
-		});
+  $.ajax({
+    url:
+      "https://api.giphy.com/v1/gifs/search?api_key=gO69tn1Qwb1dMmTpAwcc4Pb7GNWN1n5j&q=football&limit=10&offset=0&rating=PG&lang=en" +
+      team +
+      rating +
+      numberOfGIFs,
+    method: "GET"
+  }).then(function(response) {
+    response.data.forEach(function(element) {
+      newDiv = $("<div>");
+      newDiv.addClass("individual-gif-container");
+      newDiv.append("<p>rating: " + element.rating.toUpperCase() + "</p>");
+      var newImage = $(
+        "<img src = '" + element.images.fixed_height_still.url + "'>"
+      );
+      newImage.addClass("gif-image");
+      newImage.attr("state", "still");
+      newImage.attr("still-data", element.images.fixed_height_still.url);
+      newImage.attr("animated-data", element.images.fixed_height.url);
+      newDiv.append(newImage);
+      $("#gif-container").append(newDiv);
+    });
 
-		$("#gif-container").addClass("border");
-		$(".gif-image").unbind("click");
-		$(".gif-image").on("click", function () {
-			if ($(this).attr("state") === "still") {
-				$(this).attr("state", "animated");
-				$(this).attr("src", $(this).attr("animated-data"));
-			}
-			else {
-				$(this).attr("state", "still");
-				$(this).attr("src", $(this).attr("still-data"));
-			}
-		});
-	});
+    $("#gif-container").addClass("border");
+    $(".gif-image").unbind("click");
+    $(".gif-image").on("click", function() {
+      if ($(this).attr("state") === "still") {
+        $(this).attr("state", "animated");
+        $(this).attr("src", $(this).attr("animated-data"));
+      } else {
+        $(this).attr("state", "still");
+        $(this).attr("src", $(this).attr("still-data"));
+      }
+    });
+  });
 }
 
-$(document).ready(function () {
-	renderButtons();
-	$("#submit").on("click", function () {
-		event.preventDefault();
-		addButton($("#team").val());
-		$("#team").val("");
-	});
+$(document).ready(function() {
+  renderButtons();
+  $("#submit").on("click", function() {
+    event.preventDefault();
+    addButton($("#team").val());
+    $("#team").val("");
+  });
 });
